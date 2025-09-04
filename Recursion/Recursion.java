997b20047838284471eced3460402808e6495629
Rajanbikram <shahnirajan775@gmail.com>
2025-09-04 15:17:32 +0545

    first commit

---
+++ Recursion/Recursion.java
@@ -0,0 +1,29 @@
+package Java.Recursion;
+import java.util.Scanner;
+
+public class Recursion {
+
+    static void printIncresing(int n){
+
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+
+        printIncresing(n-1);
+
+        System.out.println(n);
+    }
+
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter a number");
+    int n = sc.nextInt();
+
+    System.out.println("number from 1 to " + n +":");
+    printIncresing(n);
+
+ }
+    
+}
