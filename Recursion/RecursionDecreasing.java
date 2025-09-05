953fcfa600cf781e2b51f54c5d298629f5e29189
Rajanbikram <shahnirajan775@gmail.com>
2025-09-05 16:51:48 +0545

    Decreasing number

---
+++ Recursion/RecursionDecreasing.java
@@ -0,0 +1,25 @@
+package Java.Recursion;
+
+import java.util.Scanner;
+
+public class RecursionDecreasing {
+
+    static void printDecreasing(int n){
+
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+
+        System.out.println(n);
+
+        printDecreasing(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        printDecreasing(n);
+    }
+}
