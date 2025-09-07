35b631d6cd9a99f1518b0a88bdebc4b5089e7071
Rajanbikram <shahnirajan775@gmail.com>
2025-09-07 15:49:52 +0545

    fact

---
+++ Recursion/Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+
+    static  int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+         int ans = n * factorial(n-1);
+
+         return ans;
+    }
+
+  public static void main(String[] args) {
+    System.out.println(factorial(5));
+  }
+    }
+
