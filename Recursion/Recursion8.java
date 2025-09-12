f059abe0714b016545948287be6bf9d37bc18091
Rajanbikram <shahnirajan775@gmail.com>
2025-09-12 21:37:37 +0545

    seriesSum

---
+++ Recursion/Recursion8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Recursion8 {
+
+  static int seriesSum(int n) {
+    if (n == 0) {
+      return 0;
+    }
+    if (n%2 == 0) {
+      return seriesSum(n - 1) - n;
+    } else {
+      return seriesSum(n - 1) + n;
+    }
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a num");
+    int n = sc.nextInt();
+    System.out.println(seriesSum(n));
+  }
+}
+
