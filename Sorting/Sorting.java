6157cc69daf2035128fbcf6f5767daaae81fe375
Rajanbikram <shahnirajan775@gmail.com>
2025-11-08 16:44:10 +0545

    Arranging

---
+++ Sorting/Sorting.java
@@ -0,0 +1,31 @@
+package Sorting;
+public class Sorting {
+    static void Bubblesort(int a[]) {
+
+        int n = a.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+
+                if(a[j]>a[j +1]){
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int a[] = { 1, 3, 6, 4, 7 };
+        Bubblesort(a);
+
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
